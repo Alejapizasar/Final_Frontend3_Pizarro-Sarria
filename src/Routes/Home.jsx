ea6adc50fc2d2d23903a62e37b6fb84f35a34c77
alejapizasar <alejapizasar@gmail.com>
2023-04-06 18:45:47 -0500

    Agregando data

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from '../Components/Card'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Home = () => {
+
+  const [dentist, setDentist]= useState ([])
+
+  const getDentists = async()=>{
+      const data = await fetch ("https://jsonplaceholder.typicode.com/users")
+      .then ((response) =>{
+        console.log(response)
+        return response.json()
+      })
+      setDentist(data)
+  }
+
+  useEffect(()=>{
+    getDentists()
+  }, [])
+
   return (
     <main className="" >
       <h1>Home</h1>
       <div className='card-grid'>
         {/* Aqui deberias renderizar las cards */}
+      {/*{dentist.map((dentist =>{return(<p>{dentist.name}</p>)}))}=>probando la data*/}
+      
       </div>
     </main>
   )
